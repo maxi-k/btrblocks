3d68899e8654210a30a6b502d5e5826c19d12cde
kosakseb <sebastian.kosak@tum.de>
2023-12-28 12:23:06 +0100

    restored unnecessary comments on SchemePicker.hpp

--- btrblocks/compression/SchemePicker.hpp
+++ btrblocks/compression/SchemePicker.hpp
@@ -34,12 +34,10 @@ class CSchemePicker {
   static SchemeType& chooseScheme(StatsType& stats, u8 allowed_cascading_level) {
     double max_compression_ratio = 0;
     if (MyTypeWrapper::getOverrideScheme() != autoScheme()) {
-      /// IN THAT CASE THE OVERRIDE SCHEME GETS SELECTED AND USED
       auto scheme_code = MyTypeWrapper::getOverrideScheme();
       MyTypeWrapper::getOverrideScheme() = autoScheme();
       return MyTypeWrapper::getScheme(scheme_code);
     } else {
-      /// AUTOMATIC SCHEME SELECTION
       SchemeType* preferred_scheme = nullptr;
       for (auto& scheme : MyTypeWrapper::getSchemes()) {
         if (ThreadCache::get().estimation_level != 0 || ThreadCache::get().compression_level > 1) {
@@ -185,7 +183,7 @@ class CSchemePicker {
         cerr << "!!! compressed is larger than raw: \nfor : " + comment + " - scheme = " +
                     ConvertSchemeTypeToString(static_cast<SchemeCodeType>(scheme_code))
              << " difference = " << after_size - stats.total_size << "."
-             << " Falling back to uncompressed. " << endl;
+             << " Falling back to uncompressed." << endl;
         preferred_scheme = &MyTypeWrapper::getScheme(SchemeCodeType::UNCOMPRESSED);
         scheme_code = CB(preferred_scheme->schemeType());
         after_size = preferred_scheme->compress(src, nullmap, dest, stats, allowed_cascading_level);
