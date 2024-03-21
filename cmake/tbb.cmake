4a053596bcfca210f0fb0136f80d9e74b0d8b1df
kosakseb <sebastian.kosak@tum.de>
2024-03-21 23:14:56 +0100

    fixed csvtobtr

--- cmake/tbb.cmake
+++ cmake/tbb.cmake
@@ -7,20 +7,19 @@ find_package(Git REQUIRED)
 
 # Get rapidjson
 ExternalProject_Add(
-    tbb_src
-    PREFIX "vendor/intel/tbb"
-    GIT_REPOSITORY "https://github.com/wjakob/tbb.git"
-    GIT_TAG b066defc0229a1e92d7a200eb3fe0f7e35945d95
-    TIMEOUT 10
-    BUILD_COMMAND  make
-    UPDATE_COMMAND "" # to prevent rebuilding everytime
-    INSTALL_COMMAND ""
-    CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/vendor/tbb_cpp
-    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-    -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+        tbb_src
+        PREFIX "vendor/intel/tbb"
+        GIT_REPOSITORY "https://github.com/seb711/oneTBB.git"
+        GIT_TAG master
+        TIMEOUT 10
+        BUILD_COMMAND  make
+        UPDATE_COMMAND "" # to prevent rebuilding everytime
+        INSTALL_COMMAND ""
+        CMAKE_ARGS
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:STRING=./lib
+        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} -DTBB_TEST:BOOL=OFF
 )
 
 # Prepare json
@@ -29,6 +28,7 @@ ExternalProject_Get_Property(tbb_src binary_dir)
 
 set(TBB_INCLUDE_DIR ${source_dir}/include)
 set(TBB_LIBRARY_PATH ${binary_dir}/libtbb.so)
+
 
 file(MAKE_DIRECTORY ${TBB_INCLUDE_DIR})
 
--- tools/conversion/csvtobtr.cpp
+++ tools/conversion/csvtobtr.cpp
@@ -41,183 +41,178 @@ DEFINE_string(stats, "stats.txt", "File where stats are being stored");
 DEFINE_string(compressionout,"compressionout.txt", "File where compressin times are being stored");
 DEFINE_string(typefilter, "", "Only include columns with the selected type");
 DEFINE_bool(create_binary, false, "Set if binary files are supposed to be created");
-DEFINE_bool(create_btr, true, "If false will exit after binary creation");
-DEFINE_bool(verify, false, "Verify that decompression works");
+DEFINE_bool(create_btr, false, "If false will exit after binary creation");
+DEFINE_bool(verify, true, "Verify that decompression works");
 DEFINE_int32(chunk, -1, "Select a specific chunk to measure");
 DEFINE_int32(column, -1, "Select a specific column to measure");
-DEFINE_uint32(threads, -1, "");
+DEFINE_uint32(threads, 8, "");
 // ------------------------------------------------------------------------------
 using namespace btrblocks;
 // ------------------------------------------------------------------------------
 void verify_or_die(const std::string& filename, const std::vector<InputChunk> &input_chunks) {
-    if (!FLAGS_verify) {
-        return;
+  if (!FLAGS_verify) {
+    return;
+  }
+  // Verify that decompression works
+  thread_local std::vector<char> compressed_data;
+  Utils::readFileToMemory(filename, compressed_data);
+  BtrReader reader(compressed_data.data());
+  for (SIZE chunk_i = 0; chunk_i < reader.getChunkCount(); chunk_i++) {
+    std::vector<u8> output(reader.getDecompressedSize(chunk_i));
+    bool requires_copy = reader.readColumn(output, chunk_i);
+    auto bm = reader.getBitmap(chunk_i)->writeBITMAP();
+    if (!input_chunks[chunk_i].compareContents(output.data(), bm, reader.getTupleCount(chunk_i),
+                                               requires_copy)) {
+      throw Generic_Exception("Decompression yields different contents");
     }
-    // Verify that decompression works
-    thread_local std::vector<char> compressed_data;
-    Utils::readFileToMemory(filename, compressed_data);
-    BtrReader reader(compressed_data.data());
-    for (SIZE chunk_i = 0; chunk_i < reader.getChunkCount(); chunk_i++) {
-        std::vector<u8> output(reader.getDecompressedSize(chunk_i));
-        bool requires_copy = reader.readColumn(output, chunk_i);
-        auto bm = reader.getBitmap(chunk_i)->writeBITMAP();
-        if (!input_chunks[chunk_i].compareContents(output.data(), bm, reader.getTupleCount(chunk_i),
-                                         requires_copy)) {
-            throw Generic_Exception("Decompression yields different contents");
-        }
-    }
+  }
 }
 // ------------------------------------------------------------------------------
 int main(int argc, char **argv)
 {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
-    std::string binary_path = FLAGS_binary + "/";
-    // This seems necessary to be
-    SchemePool::refresh();
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  std::string binary_path = FLAGS_binary + "/";
+  // This seems necessary to be
+  SchemePool::refresh();
 
-    if (FLAGS_threads < 1) {
-      tbb::global_control c(tbb::global_control::max_allowed_parallelism,
-                            std::thread::hardware_concurrency());
-    } else {
-      tbb::global_control c(tbb::global_control::max_allowed_parallelism,
-                            FLAGS_threads);
-    }
+  // Init TBB TODO: is that actually still necessary ?
+  // tbb::task_scheduler_init init(FLAGS_threads);
+  tbb::global_control c(tbb::global_control::max_allowed_parallelism,
+                        std::thread::hardware_concurrency());
 
-    // Load schema
-    const auto schema = YAML::LoadFile(FLAGS_yaml);
+  // Load schema
+  const auto schema = YAML::LoadFile(FLAGS_yaml);
 
-    uint64_t binary_creation_time = 0;
-    if (FLAGS_create_binary) {
-        spdlog::info("Creating binary files in " + FLAGS_binary);
-        // Load and parse CSV
-        auto start_time = std::chrono::steady_clock::now();
-        std::ifstream csv(FLAGS_csv);
-        if (!csv.good()) {
-            throw Generic_Exception("Unable to open specified csv file");
-        }
-        // parse writes the binary files
-        files::convertCSV(FLAGS_csv, schema, FLAGS_binary);
-        auto end_time = std::chrono::steady_clock::now();
-        binary_creation_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
-    }
-
-    if (!FLAGS_create_btr) {
-        return 0;
-    }
-
-    spdlog::info("Creating btr files in " + FLAGS_btr);
-
-
-    ColumnType typefilter;
-    if (FLAGS_typefilter.empty()) {
-        typefilter = ColumnType::UNDEFINED;
-    } else if (FLAGS_typefilter == "integer") {
-        typefilter = ColumnType::INTEGER;
-    } else if (FLAGS_typefilter == "double") {
-        typefilter = ColumnType::DOUBLE;
-    } else if (FLAGS_typefilter == "string") {
-        typefilter = ColumnType::STRING;
-    } else {
-        throw std::runtime_error("typefilter must be one of [integer, double, string]");
-    }
-
-    if (typefilter != ColumnType::UNDEFINED) {
-        spdlog::info("Only considering columns with type " + FLAGS_typefilter);
-    }
-
-    // Create relation
-    Relation relation = files::readDirectory(schema, FLAGS_binary.back() == '/' ? FLAGS_binary : FLAGS_binary + "/");
-    std::filesystem::path yaml_path = FLAGS_yaml;
-    relation.name = yaml_path.stem();
-
-    // Prepare datastructures for btr compression
-    //auto ranges = relation.getRanges(static_cast<SplitStrategy>(1), 9999);
-    auto ranges = relation.getRanges(SplitStrategy::SEQUENTIAL, -1);
-    assert(ranges.size() > 0);
-    Datablock datablockV2(relation);
-    std::filesystem::create_directory(FLAGS_btr);
-//    if (!std::filesystem::create_directory(FLAGS_btr)) {
-//        throw Generic_Exception("Unable to create btr output directory");
-//    }
-
-    // These counter are for statistics that match the harbook.
-    std::vector<std::atomic_size_t> sizes_uncompressed(relation.columns.size());
-    std::vector<std::atomic_size_t> sizes_compressed(relation.columns.size());
-    std::vector<u32> part_counters(relation.columns.size());
-    std::vector<ColumnType> types(relation.columns.size());
-
-    // TODO run in parallel over individual columns and handle chunks inside
-    // TODO collect statistics for overall metadata like
-    //      - total tuple count
-    //      - for every column: total number of parts
-    //      - for every column: name, type
-    // TODO chunk flag
+  uint64_t binary_creation_time = 0;
+  if (FLAGS_create_binary) {
+    spdlog::info("Creating binary files in " + FLAGS_binary);
+    // Load and parse CSV
     auto start_time = std::chrono::steady_clock::now();
-    tbb::parallel_for(SIZE(0), relation.columns.size(), [&](SIZE column_i) {
-        types[column_i] = relation.columns[column_i].type;
-        if (typefilter != ColumnType::UNDEFINED && typefilter != types[column_i]) {
-            return;
-        }
-        if (FLAGS_column != -1 && FLAGS_column != column_i) {
-            return;
-        }
-
-        std::vector<InputChunk> input_chunks;
-        std::string path_prefix = FLAGS_btr + "/" + "column" + std::to_string(column_i) + "_part";
-        ColumnPart part;
-        for (SIZE chunk_i = 0; chunk_i < ranges.size(); chunk_i++) {
-            if (FLAGS_chunk != -1 && FLAGS_chunk != chunk_i) {
-                continue;
-            }
-
-            auto input_chunk = relation.getInputChunk(ranges[chunk_i], chunk_i, column_i);
-            std::vector<u8> data = Datablock::compress(input_chunk);
-            sizes_uncompressed[column_i] += input_chunk.size;
-
-            if (!part.canAdd(data.size())) {
-                std::string filename = path_prefix + std::to_string(part_counters[column_i]);
-                sizes_compressed[column_i] += part.writeToDisk(filename);
-                part_counters[column_i]++;
-                verify_or_die(filename, input_chunks);
-                input_chunks.clear();
-            }
-
-            input_chunks.push_back(std::move(input_chunk));
-            part.addCompressedChunk(std::move(data));
-        }
-
-        if (!part.chunks.empty()) {
-            std::string filename = path_prefix + std::to_string(part_counters[column_i]);
-            sizes_compressed[column_i] += part.writeToDisk(filename);
-            part_counters[column_i]++;
-            verify_or_die(filename, input_chunks);
-            input_chunks.clear();
-        }
-    });
-
-    Datablock::writeMetadata(FLAGS_btr + "/metadata", types, part_counters, ranges.size());
-    std::ofstream stats_stream(FLAGS_stats);
-    size_t total_uncompressed = 0;
-    size_t total_compressed = 0;
-    stats_stream << "Column,uncompressed,compressed\n";
-    for (SIZE col=0; col  < relation.columns.size(); col++) {
-        total_uncompressed += sizes_uncompressed[col];
-        total_compressed += sizes_compressed[col];
-
-        stats_stream << relation.columns[col].name << "," << sizes_uncompressed[col] << "," << sizes_compressed[col] << "\n";
+    std::ifstream csv(FLAGS_csv);
+    if (!csv.good()) {
+      throw Generic_Exception("Unable to open specified csv file");
     }
+    // parse writes the binary files
+    files::convertCSV(FLAGS_csv, schema, FLAGS_binary);
     auto end_time = std::chrono::steady_clock::now();
-    uint64_t btr_creation_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    binary_creation_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+  }
 
-    stats_stream << "Total," << total_uncompressed << "," << total_compressed << std::endl;
+  if (!FLAGS_create_btr) {
+    return 0;
+  }
 
-    std::ofstream compressionout_stream(FLAGS_compressionout);
-    double binary_creation_time_seconds = static_cast<double>(binary_creation_time) / 1e6;
-    double btr_creation_time_seconds = static_cast<double>(btr_creation_time) / 1e6;
 
-   compressionout_stream << "binary: " << binary_creation_time_seconds
+  ColumnType typefilter;
+  if (FLAGS_typefilter.empty()) {
+    typefilter = ColumnType::UNDEFINED;
+  } else if (FLAGS_typefilter == "integer") {
+    typefilter = ColumnType::INTEGER;
+  } else if (FLAGS_typefilter == "double") {
+    typefilter = ColumnType::DOUBLE;
+  } else if (FLAGS_typefilter == "string") {
+    typefilter = ColumnType::STRING;
+  } else {
+    throw std::runtime_error("typefilter must be one of [integer, double, string]");
+  }
+
+  if (typefilter != ColumnType::UNDEFINED) {
+    spdlog::info("Only considering columns with type " + FLAGS_typefilter);
+  }
+
+  // Create relation
+  Relation relation = files::readDirectory(schema, FLAGS_binary.back() == '/' ? FLAGS_binary : FLAGS_binary + "/");
+  std::filesystem::path yaml_path = FLAGS_yaml;
+  relation.name = yaml_path.stem();
+
+  // Prepare datastructures for btr compression
+  //auto ranges = relation.getRanges(static_cast<SplitStrategy>(1), 9999);
+  auto ranges = relation.getRanges(SplitStrategy::SEQUENTIAL, -1);
+  assert(ranges.size() > 0);
+  Datablock datablockV2(relation);
+  std::filesystem::create_directory(FLAGS_btr);
+  //    if (!std::filesystem::create_directory(FLAGS_btr)) {
+  //        throw Generic_Exception("Unable to create btr output directory");
+  //    }
+
+  // These counter are for statistics that match the harbook.
+  std::vector<std::atomic_size_t> sizes_uncompressed(relation.columns.size());
+  std::vector<std::atomic_size_t> sizes_compressed(relation.columns.size());
+  std::vector<u32> part_counters(relation.columns.size());
+  std::vector<ColumnType> types(relation.columns.size());
+
+  // TODO run in parallel over individual columns and handle chunks inside
+  // TODO collect statistics for overall metadata like
+  //      - total tuple count
+  //      - for every column: total number of parts
+  //      - for every column: name, type
+  // TODO chunk flag
+
+  auto start_time = std::chrono::steady_clock::now();
+  tbb::parallel_for(SIZE(0), relation.columns.size(), [&](SIZE column_i) {
+    types[column_i] = relation.columns[column_i].type;
+    if (typefilter != ColumnType::UNDEFINED && typefilter != types[column_i]) {
+      return;
+    }
+    if (FLAGS_column != -1 && FLAGS_column != column_i) {
+      return;
+    }
+
+    std::vector<InputChunk> input_chunks;
+    std::string path_prefix = FLAGS_btr + "/" + "column" + std::to_string(column_i) + "_part";
+    ColumnPart part;
+    for (SIZE chunk_i = 0; chunk_i < ranges.size(); chunk_i++) {
+      if (FLAGS_chunk != -1 && FLAGS_chunk != chunk_i) {
+        continue;
+      }
+
+      auto input_chunk = relation.getInputChunk(ranges[chunk_i], chunk_i, column_i);
+      std::vector<u8> data = Datablock::compress(input_chunk);
+      sizes_uncompressed[column_i] += input_chunk.size;
+      if (!part.canAdd(data.size())) {
+        std::string filename = path_prefix + std::to_string(part_counters[column_i]);
+        sizes_compressed[column_i] += part.writeToDisk(filename);
+        part_counters[column_i]++;
+        verify_or_die(filename, input_chunks);
+        input_chunks.clear();
+      }
+
+      input_chunks.push_back(std::move(input_chunk));
+      part.addCompressedChunk(std::move(data));
+    }
+
+    if (!part.chunks.empty()) {
+      std::string filename = path_prefix + std::to_string(part_counters[column_i]);
+      sizes_compressed[column_i] += part.writeToDisk(filename);
+      part_counters[column_i]++;
+      verify_or_die(filename, input_chunks);
+      input_chunks.clear();
+    }
+  });
+
+  Datablock::writeMetadata(FLAGS_btr + "/metadata", types, part_counters, ranges.size());
+  std::ofstream stats_stream(FLAGS_stats);
+  size_t total_uncompressed = 0;
+  size_t total_compressed = 0;
+  stats_stream << "Column,uncompressed,compressed\n";
+  for (SIZE col=0; col  < relation.columns.size(); col++) {
+    total_uncompressed += sizes_uncompressed[col];
+    total_compressed += sizes_compressed[col];
+
+    stats_stream << relation.columns[col].name << "," << sizes_uncompressed[col] << "," << sizes_compressed[col] << "\n";
+  }
+  auto end_time = std::chrono::steady_clock::now();
+  uint64_t btr_creation_time = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+
+  std::cout << "Total," << total_uncompressed << "," << total_compressed << std::endl;
+
+  std::ofstream compressionout_stream(FLAGS_compressionout);
+  double binary_creation_time_seconds = static_cast<double>(binary_creation_time) / 1e6;
+  double btr_creation_time_seconds = static_cast<double>(btr_creation_time) / 1e6;
+
+  compressionout_stream << "binary: " << binary_creation_time_seconds
                         << " btr: " << btr_creation_time_seconds
                         << " total: " << (binary_creation_time_seconds + btr_creation_time_seconds)
                         << " verify: " << FLAGS_verify << std::endl;
-    return 0;
+  return 0;
 }
